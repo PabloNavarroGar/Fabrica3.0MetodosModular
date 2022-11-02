df417533f270e3508eb78ca89b66c22718015373
PabloNavarro <pnavgarb@g.educaand.es>
2022-11-02 13:49:48 +0100

    Primera subida del ejercicio

---
+++ src/main/java/fabricaMetodos/metodosFabrica.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fabricaMetodos;
+
+/**
+ *
+ * @author pablo
+ */
+public class metodosFabrica {
+
+//    1º metodo para mostarmuenu inicial
+//    2º metodo de leer codigos
+//    3º filtrarcodigos                
+//    4º leerMateriaPrima
+//    5º FiltrarMateriaPrima
+//    6º leemanodeobra
+//    7º filtrarManoObra
+//    8º CosteProduccion
+//    9º Calcular Precioventa
+//    10º calcular unidadesBeneficio
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
